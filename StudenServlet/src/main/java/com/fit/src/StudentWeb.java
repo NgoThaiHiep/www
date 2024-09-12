b7c174ec3f0a77c115550fb51346d5ac58e288fb
hiepgit2812 <21081841.hiep@student.iuh.edu.vn>
2024-09-12 18:17:43 +0700

    add

--- StudenServlet/src/main/java/com/fit/src/StudentWeb.java
+++ StudenServlet/src/main/java/com/fit/src/StudentWeb.java
@@ -1,11 +1,14 @@
 package com.fit.src;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+
+import com.fit.entity.Student;
 
 /**
  * Servlet implementation class StudentWeb
@@ -33,7 +36,22 @@ public class StudentWeb extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		response.setContentType("text/html");
+		
+		String fName = request.getParameter("txtFName");
+		String lName = request.getParameter("txtLName");
+		
+		
+		Student sv = new Student();
+		sv.setfName(fName);
+		sv.setlName(lName);
+		
+		
+		request.setAttribute("student", sv);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("studentJSP.jsp");
+		rd.forward(request, response);
+		
 	}
 
 }
--- StudenServlet/src/main/webapp/studentJSP.jsp
+++ StudenServlet/src/main/webapp/studentJSP.jsp
@@ -1,12 +1,17 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@page import="com.fit.entity.Student"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" 
+    pageEncoding="UTF-8"%> 
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="ISO-8859-1">
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"> 
 <title>Insert title here</title>
 </head>
 <body>
-	
+	<%
+		Student sv = new Student();
+		sv = (Student)request.getAttribute("student");
+		out.println("First name:" + sv.getfName() );
+	%>
 </body>
 </html>
